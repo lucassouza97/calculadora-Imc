2e41620343e29bf347895cc2a1338685cbbafb76
Lucas Souza <l.lucassouza@hotmail.com>
2020-02-04 22:46:42 -0300

    melhorias

--- App.js
+++ App.js
@@ -1,78 +1,136 @@
 import React, { Component } from 'react'
-import { Text, View, TextInput, StyleSheet, TouchableOpacity, Button } from 'react-native'
+import { Text, View, TextInput, StyleSheet, TouchableOpacity, Button, Image, Linking, Alert } from 'react-native'
 
 export default class App extends Component {
 
     constructor(props){
         super(props);
-        this.state={altura: 0, peso: 0, resultado: 0, resultadoText: ''}
-        this.calculaImc = this.calculaImc.bind(this);
+        this.state = { 
+            altura: '', 
+            peso: '', 
+            resultado: '', 
+            resultadoText: '',
+            showResult: false,
+        }
+       this.calculaImc = this.calculaImc.bind(this);
     }
 
     calculaImc(){
-     let s = this.state;
-     
-     s.resultado = Math.floor((s.peso / (s.altura * s.altura)));
+        var x; //Variavel global
+        let y; //Variavel de escopo
+        const altura = parseFloat(this.state.altura)
 
-     if(s.resultado < 17){
-        s.resultadoText = 'Você está muito abaixo do peso';
-     } else if(s.resultado >= 17 && s.resultado <= 18.49){
-         s.resultadoText = 'Você está abaixo do peso';
-     } else if(s.resultado >= 18.5 && s.resultado <=24.99){
-         s.resultadoText = 'Você está em seu peso ideal';
-     } else if(s.resultado >= 25 && s.resultado <=29.99){
-        s.resultadoText = 'Você está acima do peso';
-    }  else if(s.resultado >= 30 && s.resultado <=34.99){
-        s.resultadoText = 'Você está no nível de Obesidade I' 
-    }  else if(s.resultado >= 35 && s.resultado <=39.99){
-        s.resultadoText = 'Você está no nível de Obesidade II(Severa)' 
-    }  else if(s.resultado >= 40){
-        s.resultadoText = 'Você está no nível de Obesidade III(Mórbida)' 
-    };
-    
-        this.setState(s);
+        this.state.showResult = true;
+        this.state.resultado = Math.floor((parseFloat(this.state.peso) / (altura * altura)));
 
+        
+        //  switch(this.state.resultado){
+        //      case this.state.resultado < 17: 
+        //      this.setState({ resultadoText: 'Você está muito abaixo do peso' })
+        //      break;
+
+        //      case this.state.resultado >= 17 && this.state.resultado <= 18.49: 
+        //      this.setState({ resultadoText: 'Você está abaixo do peso' }) 
+        //      break;
+
+        //      case this.state.resultado >= 25 && this.state.resultado <=29.99: 
+        //      this.setState({ resultadoText: 'Você está em seu peso ideal' })  
+        //      break;
+
+        //      case this.state.resultado >= 30 && this.state.resultado <=34.99: 
+        //      this.setState({ resultadoText: 'Você está no nível de Obesidade I' })  
+        //      break;
+
+        //      case this.state.resultado >= 35 && this.state.resultado <=39.99: 
+        //      this.setState({ resultadoText: 'Você está no nível de Obesidade II(Severa)'})
+        //      break;
+
+        //      case this.state.resultado >= 40: 
+        //      this.setState({ resultadoText: 'Você está no nível de Obesidade III(Mórbida)' }) 
+        //      break;
+
+        //      default:
+        //          alert("i dont know such values")
+
+        //  }
+
+        if(this.state.resultado < 17){
+            this.setState({ resultadoText: 'Você está muito abaixo do peso' })
+        } else if(this.state.resultado >= 17 && this.state.resultado <= 18.49){
+            this.setState({ resultadoText: 'Você está abaixo do peso' }) 
+        } else if(this.state.resultado >= 18.5 && this.state.resultado <=24.99){
+            this.setState({ resultadoText: 'Você está em seu peso ideal' }) 
+        } else if(this.state.resultado >= 25 && this.state.resultado <=29.99){
+            this.setState({ resultadoText: 'Você está acima do peso' }) 
+        }  else if(this.state.resultado >= 30 && this.state.resultado <=34.99){
+            this.setState({ resultadoText: 'Você está no nível de Obesidade I' }) 
+        }  else if(this.state.resultado >= 35 && this.state.resultado <=39.99){
+            this.setState({ resultadoText: 'Você está no nível de Obesidade II(Severa)'  }) 
+        }  else if(this.state.resultado >= 40){
+            this.setState({ resultadoText: 'Você está no nível de Obesidade III(Mórbida)' }) 
+        };
     }
+
     render() {
         return (
             <View style={styles.body}>
-            <View>
-                <Text> cálculo imc </Text>
-
-                <View>
-                    <Text> Digite seu peso:</Text>
-                <TextInput style ={styles.input} placeholder='Peso' placeholderTextColor='black' 
-                keyboardType="numeric" onChangeText={peso => this.setState({peso})} value={this.state.peso}/>
-                <TextInput style ={styles.input} placeholder='Altura' placeholderTextColor='black'  
-                keyboardType="numeric" onChangeText={altura => this.setState({altura})} value={this.state.altura}/>
+                <Image style={styles.logoStyle}source={require('./src/images/smartwatch.png')} />
+                <Text style={styles.titleStyle}> cálculo imc </Text>
+                <Text style={{color:'white', fontSize: 17, top: 20, right: 45}}> Digite seu peso:</Text>
+                <View style={styles.viewInputText}>
+                    <Image style={{width: 40, height: 40, }}source={require('./src/images/balanca.png')}/>
+                    <TextInput 
+                        style ={styles.input} 
+                        placeholder="Peso (ex: 70.5)" 
+                        placeholderTextColor='black' 
+                        keyboardType={'numeric'} 
+                        onChangeText={peso => this.setState({peso})} 
+                        value={this.state.peso}
+                    />
+                </View>
+                <Text style={{color:'white', fontSize: 17, top: 20, right: 43}}> Digite sua altura:</Text> 
+                <View style={styles.viewInputText}>
+                    <Image style={{width: 40, height: 40}} source={require('./src/images/altura.png')}/>
+                    <TextInput 
+                        style={styles.input} 
+                        placeholder='Altura (ex: 1.80)' 
+                        placeholderTextColor='black'  
+                        keyboardType={'numeric'} 
+                        onChangeText={altura => this.setState({altura})} 
+                        value={this.state.altura}
+                    />
                 </View>
 
-
+                <Button title='Calcule' onPress={this.calculaImc} />
+                
+                {this.state.showResult && <Text style={styles.resultStyle}>Seu IMC é: {this.state.resultado} - {this.state.resultadoText} </Text>}
+                
+                <View style={{top: 94, left: 185}}>
+                    <TouchableOpacity onPress={() => {Linking.openURL('https://pt.wikipedia.org/wiki/%C3%8Dndice_de_massa_corporal')}}> 
+                        <Image  style={styles.imageInfo} source={require('./src/images/info.png')}/>
+                    </TouchableOpacity>
+                </View>
             </View>
 
-                <View>
-
-                    <Button title='Calcule aqui' onPress={this.calculaImc}/>
-                    
-                <Text>Resultado: Seu imc é: {this.state.resultado} - {this.state.resultadoText} </Text>
-                </View>
-                </View>
         )
     }
 }
 
-const styles = StyleSheet.create ({
-    body:{
-    flex:1,
-    flexDirection: 'column',
-    backgroundColor:  '#ff6348',
+const styles = StyleSheet.create({
+    body: {
+        flex:1,
+        flexDirection: 'column',
+        backgroundColor:  '#ff6348',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-
-    viewInputText:{
-    flexDirection: 'row'
+    viewInputText: {
+        flexDirection: 'row',
+        paddingTop: 10,
+        alignItems: 'center',
+        justifyContent: 'center'
     },
-
-    input:{
+    input: {
         margin: 15,
         height: 40,
         width: 250,
@@ -80,7 +138,25 @@ const styles = StyleSheet.create ({
         borderWidth: 1,
         backgroundColor: 'white',
         borderRadius: 6
-}
+    },
+    logoStyle: {
+        width: 150,
+        height: 150,
+        alignItems: 'center',
+        justifyContent: 'center',     
+    },
+    titleStyle: {
+        fontSize: 20,
+        color: 'white'
+    },
+    resultStyle: {
+        fontSize: 20,
+        color: 'white',
+    },
+    imageInfo: {
+        width: 38,
+        height: 40,
+    }
 })
     
 
